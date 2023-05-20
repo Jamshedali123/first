5f5496d3f13a743c3998ced61bde47ef44dc1e46
Jamshedali123 <ajamshed456@gmail.com>
2023-05-20 13:16:24 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+def srujana(request):
+    return HttpResponse('<marquee><b><h1>thinnaverra khuykfsabulbvkwulum</h1></b></marquee>')
